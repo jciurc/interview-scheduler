919d0e40826dcb424c6aadae3f1a8e8e2b67c573
symphony <jciurc@gmail.com>
2022-05-17 17:48:10 -0600

    set up button component to handle props. storybook styling working. pass 7/7 button jest tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-  return <></>;
+  let buttonClass = "button";
+  buttonClass += props.confirm ? ' button--confirm' : '';
+  buttonClass += props.danger ? ' button--danger' : '';
+  buttonClass += props.someOtherClass ? ' button--some-other-class' : '';
+
+  return <button
+    className={buttonClass}
+    onClick={props.onClick}
+    disabled={props.disabled}
+  >
+    {props.children}
+  </button>;
 }
