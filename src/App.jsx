1ba791e8a680cee7cc90aa1118148a0b05aeae84
symphony <jciurc@gmail.com>
2022-05-18 16:57:05 -0600

    set up appointment components in App with test data

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,54 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+const parsedAppointments = Object.values(appointments).map((apt) => {
+  return (<Appointment
+    key={apt.id}
+    {...apt}
+  />);
+});
+
+
+// = main App component =
 export default function App() {
   const [day, setDay] = useState('Monday');
 
@@ -46,7 +94,7 @@ export default function App() {
 
       </section>
       <section className="schedule">
-        <Appointment time="12" />
+        {parsedAppointments}
       </section>
     </main>
   );
