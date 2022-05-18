8b3f048fdb4dabc7cb44c1f13c3f044f89beea71
symphony <jciurc@gmail.com>
2022-05-18 15:37:52 -0600

    add Show and Confirm components to Appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Empty from "./Empty";
+import Show from "./Show";
 import './styles.scss';
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
+      <Header time={props.time} />
       {props.time && <p>Appointment for {props.time}</p>}
 
+      <Show />
       <Empty />
     </article>
   );
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 //  = button =
@@ -160,3 +162,13 @@ storiesOf("Appointment", module)
   .add("Appointment with time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show
+    student="hello there"
+    interviewer={interviewers[0]}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />)
+  .add("Confirm", () => <Confirm
+    onCancel={action("onCancel")}
+    onConfirm={action("onConfirm")}
+  />);
