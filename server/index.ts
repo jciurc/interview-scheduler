3699b9a97391aa676b74ce793febd195db7616ca
symphony <jciurc@gmail.com>
2022-07-22 16:40:04 -0700

    set up index file and imports. create routes folder and files. uninstall body-parser package (it's built into express)

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,36 @@
-import express, { Express, Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
+
+// get env variables
 import dotenv from 'dotenv';
+const ENV = process.env.NODE_ENV || 'development';
+const PATH = path.resolve(__dirname, './.env.' + ENV);
+dotenv.config({ path: PATH });
 
-dotenv.config();
+// modules
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import db from './db';
 
+// routes
+import days from './routes/days';
+import appointments from './routes/appointments';
+import interviewers from './routes/interviewers';
+
+// config
+const PORT = process.env.PORT || 8001;
 const app: Express = express();
-const port = process.env.PORT || 8001;
+
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hi');
+  res.send('Hi?');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT} in ${ENV} mode.`);
 });
