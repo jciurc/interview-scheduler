743fe0a1741080f34fc552508e9a8853cc33118e
symphony <jciurc@gmail.com>
2022-07-25 22:40:02 -0700

    install font awesome react packages. working fa icons and toggle button

--- client/src/components/ToggleButton.tsx
+++ client/src/components/ToggleButton.tsx
@@ -1,17 +1,22 @@
 import './styles/toggleButton.scss';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 interface IToggleProps {
   toggleDark: ToggleDark;
 };
 
 // trying out class component for fun
-export default class ToggleButton extends React.Component {
+export default class ToggleButton extends React.Component<IToggleProps> {
   constructor(props: IToggleProps) {
     super(props);
   };
 
   render(): React.ReactNode {
-    return (<section className='toggle' onClick={this.props.toggleDark}>hello</section>)
+    return (<section className='toggle' onClick={this.props.toggleDark}>
+      <FontAwesomeIcon icon={faMoon} className='fa-moon'/>
+      <FontAwesomeIcon icon={faSun} className='fa-sun'/>
+    </section>)
   }
 };
