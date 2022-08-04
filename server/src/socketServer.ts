af7016bb71255b3af85ca08364834fa7d8d16dd9
symphony <jciurc@gmail.com>
2022-08-04 10:01:46 -0700

    create socketserver file. update readme formatting

--- server/src/socketServer.ts
+++ server/src/socketServer.ts
@@ -1,4 +1,12 @@
 import { Server } from 'http'; // types
 import * as socketio from 'socket.io';
 
-export const listen = (httpServer: Server) => new socketio.Server(httpServer);
+export const listen = (httpServer: Server) => {
+  const server = new socketio.Server(httpServer);
+
+  server.on('connect', () => {
+
+  });
+
+  return server;
+};
